73d87f84c6859ee8c21692ee4ec2a06a944591ea
Dayun Kang <0411yun@kookmin.ac.kr>
2020-06-19 12:50:09 +0900

    Commit WSGI files

---
+++ tem.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+    <body>
+	<form action="">
+	    num1 = <input type="number" name="a"><br>
+	    num2 = <input type="number" name="b"><br>
+	    <input type="submit">
+	</form>
+	<p>
+	sum : %(sum)s</br>
+	product : %(product)s</br>
+	</p>
+    </body>
+</html>
+""" 
